c1f63cdd51188491a831c3a600474518f28f6612
jinInoue-rrr <inojin0326@icloud.com>
2022-05-10 06:27:49 +0900

    added details

--- linear_regression.py
+++ linear_regression.py
@@ -5,17 +5,17 @@ from scipy import linalg
 class LinReg:
     def __init__(self):
         self.beta_ = None
-
-    def fit(self, X,y):
+        
+    def fit(self,X,y):
         X = np.array(X)
         y = np.array(y)
-        xtx = np.dot(X.T, X)
-        xty = np.dot(X.T,y)
-        self.beta_ = linalg.solve(xtx,xty)
+        xtx = np.dot(X.T, X)#X^TX
+        xty = np.dot(X.T,y)#X^Ty
+        self.beta_ = linalg.solve(xtx,xty)#FOCを解く
     
     def pred(self,X):
         if X.ndim == 1:
             X = X.reshape(1,-1)
         X = np.array(X)
-        return np.dot(X,self.beta_)
+        return np.dot(X,self.beta_)#X\beta^hat = y_pred
 
